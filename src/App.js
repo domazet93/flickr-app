30d223c431a9af49841bf97cd4d08028a0763c1b
Toni Domazet <toni.domaz93@hotmail.com>
2018-11-26 21:20:17 +0000

    flickr service - get photos after App has mounted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
-
+import axios from "./axios/axios-flickr";
 class App extends Component {
+
+  state = {
+    isLoading: false,
+    photos: {}
+  }
+
+  componentDidMount() { 
+    this.setState({ isLoading: true, pictures: [] }, () => {
+      axios
+        .get("/", {
+          params: {
+            method: "flickr.photos.getRecent",
+            format: "json",
+            nojsoncallback: 1,
+            api_key: process.env.REACT_APP_API_KEY
+          }
+        })
+        .then(res => {
+          this.setState({
+            photos: res.data.photos
+          })
+        })
+        .catch(error => null)
+        .finally(() => {
+          this.setState({ isLoading: false });
+        });
+    });
+  }
+
   render() {
+    let photos = null;
+    if(this.state.isLoading) {
+      photos = "Loading..."
+    } else {
+      photos = "Loaded"
+    }
+
     return (
-      <p>Flickr App</p>       
+      <div>
+        <p>Flickr App</p> 
+        <p>{ photos }</p>
+      </div>
     );
   }
 }
