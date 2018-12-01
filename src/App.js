e02aef635284416f844d21d7a8af2dc1ad13ce0d
Toni Domazet <toni.domaz93@hotmail.com>
2018-12-01 11:19:09 +0000

    install react-infinite-scroller to handle load photos on scroll

--- src/App.js
+++ src/App.js
@@ -1,37 +1,58 @@
 import React, { Component } from 'react';
 import classes from "./App.module.scss";
+import FlickService from "./services/api/flickr.service"
+
+import InfiniteScroll from "react-infinite-scroller";
 
 import Spinner from "./components/UI/Spinner/Spinner";
 import Input from "./components/UI/Input/Input";
 import Button from "./components/UI/Button/Button";
 import Card from "./components/UI/Card/Card";
 import NoData from "./components/UI/NoData/NoData"
-import FlickService from "./services/api/flickr.service"
 
 class App extends Component {
 
   state = {
     isLoading: false,
     gallery: [],  
-    description: ""
+    description: "",
+    hasMore: true
   }
 
   componentDidMount() { 
-    this.setState({ isLoading: true, gallery: [] }, () => {
-        this.getRecentPhotoAPI()
-        .then((res) => {
-          this.setState({
-            gallery: res.data.photos.photo
-          }, async () => {          
-            let gallery = await this.getGalleryWithOwnersInfo()
-            this.setState({ gallery }); 
-          }); 
-        })
-        .catch(error => null)
-        .finally(() => {
-          this.setState({ isLoading: false });
-        });
+    this.setState({ isLoading: true }, () => {
+      this.getPhotos()
     });
+  }
+
+  getPhotos = () => {   
+    this.getPhotosAPI()
+      .then((res) => {
+        this.setState({
+          gallery: res.data.photos.photo
+        }, async () => {          
+          let gallery = await this.getGalleryWithOwnersInfo()
+          this.setState({ gallery }); 
+        }); 
+      })
+      .catch(error => null)
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  }
+
+  loadMorePhotos = () => {
+      this.getPhotosAPI()
+      .then((res) => {
+        this.setState({
+          gallery: res.data.photos.photo
+        }, async () => {          
+          let gallery = await this.getGalleryWithOwnersInfo()
+          let result = [ ...this.state.gallery, ...gallery]
+          this.setState({ gallery: result, hasMore: true }); 
+        });
+      })
+      .catch(error => null)
   }
 
   handleQueryChange = event => {
@@ -39,28 +60,10 @@ class App extends Component {
   };
 
   handleQueryKeyPress = event => {
-    if (event.key === 'Enter') {
-      this.handleQueryEvent()
+    if (event.key === 'Enter' && this.state.description) {
+      this.getPhotos()
     }      
   };
-
-  handleQueryEvent = () => {
-    this.setState({ isLoading: true, gallery: [] }, () => {
-      this.getPhotosAPI()
-      .then((res) => {
-        this.setState({
-          gallery: res.data.photos.photo
-        }, async () => {          
-          let gallery = await this.getGalleryWithOwnersInfo()
-          this.setState({ gallery }); 
-        });
-      })
-      .catch(error => null)
-      .finally(() => {
-        this.setState({ isLoading: false });
-      });
-    })
-  }
 
   getGalleryWithOwnersInfo = () => {
     let gallery =  [ ...this.state.gallery ]
@@ -69,15 +72,6 @@ class App extends Component {
       person: await this.getPersonInfoAPI(photo)
     })) ;
     return Promise.all(promises);
-  }
-
-  getRecentPhotoAPI = () => {
-    return FlickService
-    .getRecentPhotos("/", {
-      params: {
-        per_page: 20
-      }
-    })
   }
 
   getPhotosAPI = () => {
@@ -106,10 +100,11 @@ class App extends Component {
       gallery = this.state.gallery.length ? (
         this.state.gallery.map((photo, $index) => (
         <div className={ "flex-lg-3 flex-md-6 flex-xs-1 m-2" } key={$index}>
-          <Card key={$index}       
-                title={photo.title}
-                image={`http://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
-                desc="desc"/>
+          <Card 
+            key={$index}       
+            title={photo.title}
+            image={`http://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+            desc="desc"/>
         </div>
         
         ))
@@ -123,18 +118,29 @@ class App extends Component {
         <h3>flickr</h3> 
         <div className="d-flex flex-row">
           <div className="d-flex flex-fill">
-            <Input changed={ this.handleQueryChange }
-                    keypress={ this.handleQueryKeyPress } />  
+            <Input
+                changed={ this.handleQueryChange }
+                keypress={ this.handleQueryKeyPress } />  
             <div className="ml-2">
-              <Button type="Success" clicked={ this.handleQueryEvent }>Search</Button>
+              <Button 
+                  type="Success" 
+                  isDisabled={!this.state.description} 
+                  clicked={ this.getPhotos }>Search</Button>
             </div>          
           </div>
           <div className="d-flex flex-fill">
           
           </div>
         </div> 
-        <div className="d-flex flex-wrap justify-content-center">
-        <div className={`${classes.App} mt-3`}>{ gallery }</div>
+        <div className="d-flex flex-wrap justify-content-center">   
+          <InfiniteScroll 
+              initialLoad={false}
+              className={`${classes.App} mt-3`}
+              hasMore={this.state.hasMore}
+              pageStart={0}
+              loadMore={this.loadMorePhotos}>
+            {gallery} 
+          </InfiniteScroll>
         </div>
       </div> 
     );
